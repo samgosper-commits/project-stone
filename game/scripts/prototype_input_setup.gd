21633d9a813f7b4d4b795ec52062321dbc569485
samgosper-commits <samgosper@gmail.com>
2026-09-24 04:26:37 +1000

    Fix input bootstrap parse error

--- game/scripts/prototype_input_setup.gd
+++ game/scripts/prototype_input_setup.gd
@@ -1,31 +1,25 @@
 extends Node
 
 const ACTIONS := ["left","right","up","down","jump","attack","action","evade","power"]
-const BUTTONS := {
-	"jump": JOY_BUTTON_A,
-	"attack": JOY_BUTTON_X,
-	"action": JOY_BUTTON_B,
-	"evade": JOY_BUTTON_Y,
-	"power": JOY_BUTTON_RIGHT_SHOULDER
-}
+const BUTTONS := {"jump":JOY_BUTTON_A,"attack":JOY_BUTTON_X,"action":JOY_BUTTON_B,"evade":JOY_BUTTON_Y,"power":JOY_BUTTON_RIGHT_SHOULDER}
 
-func _ready() -> void:
+func _ready()->void:
 	for p in range(1,5):
 		for action in ACTIONS:
-			var name := "p%d_%s" % [p,action]
-			if not InputMap.has_action(name): InputMap.add_action(name)
-		_add_pad_bindings(p-1,name,action)
+			var action_name:="p%d_%s"%[p,action]
+			if not InputMap.has_action(action_name):InputMap.add_action(action_name)
+			_add_pad_bindings(p-1,action_name,action)
 	_add_keyboard()
 
-func _add_pad_bindings(device:int, action_name:String, action:String) -> void:
+func _add_pad_bindings(device:int,action_name:String,action:String)->void:
 	if action=="left" or action=="right":
-		var e:=InputEventJoypadMotion.new(); e.device=device; e.axis=JOY_AXIS_LEFT_X; e.axis_value=-1.0 if action=="left" else 1.0; InputMap.action_add_event(action_name,e)
+		var e:=InputEventJoypadMotion.new();e.device=device;e.axis=JOY_AXIS_LEFT_X;e.axis_value=-1.0 if action=="left" else 1.0;InputMap.action_add_event(action_name,e)
 	elif action=="up" or action=="down":
-		var e:=InputEventJoypadMotion.new(); e.device=device; e.axis=JOY_AXIS_LEFT_Y; e.axis_value=-1.0 if action=="up" else 1.0; InputMap.action_add_event(action_name,e)
+		var e:=InputEventJoypadMotion.new();e.device=device;e.axis=JOY_AXIS_LEFT_Y;e.axis_value=-1.0 if action=="up" else 1.0;InputMap.action_add_event(action_name,e)
 	elif BUTTONS.has(action):
-		var b:=InputEventJoypadButton.new(); b.device=device; b.button_index=BUTTONS[action]; InputMap.action_add_event(action_name,b)
+		var b:=InputEventJoypadButton.new();b.device=device;b.button_index=BUTTONS[action];InputMap.action_add_event(action_name,b)
 
 func _add_keyboard()->void:
 	var keys={"p1_left":KEY_A,"p1_right":KEY_D,"p1_up":KEY_W,"p1_down":KEY_S,"p1_jump":KEY_SPACE,"p1_attack":KEY_J,"p1_action":KEY_K,"p1_evade":KEY_L,"p1_power":KEY_I}
-	for name in keys:
-		var e:=InputEventKey.new(); e.physical_keycode=keys[name]; InputMap.action_add_event(name,e)
+	for action_name in keys:
+		var e:=InputEventKey.new();e.physical_keycode=keys[action_name];InputMap.action_add_event(action_name,e)
